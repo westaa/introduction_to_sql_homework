5908f03f8818627ace6a3ec70b230f5f22fb11de
Andrew West <andrewwest@Andrews-MacBook-Pro.local>
2016-05-27 09:23:48 -0600

    files are now executable from command line using code in the homework file

---
+++ all_of_the_other_commands.sql
@@ -0,0 +1,15 @@
+CREATE TABLE urls (id serial, original_url VARCHAR(50)
+NOT NULL, count INT  DEFAULT 0, PRIMARY KEY(id));
+
+INSERT INTO urls (original_url, count) VALUES ('www.pregame.com', '1');
+INSERT INTO urls (original_url, count) VALUES ('www.pickmonitor.com', '2');
+INSERT INTO urls (original_url, count) VALUES ('www.nba.com', '3');
+INSERT INTO urls (original_url, count) VALUES ('www.nfl.com', '4');
+INSERT INTO urls (original_url, count) VALUES ('www.mlb.com', '5');
+
+SELECT * FROM urls;
+SELECT original_url FROM urls;
+SELECT * FROM urls WHERE id=2;
+SELECT * FROM urls WHERE original_url='www.pregame.com';
+UPDATE urls SET COUNT='12' WHERE ID=2;
+DELETE FROM urls WHERE original_url='www.nba.com';
---
+++ create_url_shortener_database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE url_shortener_aaw;
